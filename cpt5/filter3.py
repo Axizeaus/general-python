da95d36c77c53ba1303db83ae5f27af3fd3e5ef8
Axizeaus <axizeaus@gmail.com>
2022-04-27 12:02:29 +0630

    using filter in list comprehensions

---
+++ cpt5/filter3.py
@@ -0,0 +1,9 @@
+from math import sqrt 
+
+mx = 10
+triples = [(a,b, int(c))
+           for a in range(1,mx)
+           for b in range(a, mx)
+           if (c := sqrt(a ** 2 + b ** 2)).is_integer()]
+
+print(triples)
--- cpt5/filtering_a_comprehension.py
+++ cpt5/filtering_a_comprehension.py
@@ -2,16 +2,34 @@ from math import sqrt
 
 mx = 10
 
-triples = [(a,b, sqrt(a**2 + b**2)) 
-           for a in range(1, mx) 
+triples = [(a, b, sqrt(a**2 + b**2))
+           for a in range(1, mx)
            for b in range(a, mx)]
 
 print(triples)
 
-triples = list(
+print('=' * 10)
+
+triples2 = filter(lambda triple: triple[2].is_integer(), triples)
+
+print(triples2)
+print('=' * 10)
+
+
+triples3 = list(
     filter(lambda triple: triple[2].is_integer(), triples)
 )
 
-print(triples)
+print(triples3)
+print('=' * 10)
+
+
+triples4 = list(
+    map(lambda triple: triple[:2] + (int(triple[2]),), triples)
+)
+
+print(triples4)
+print('=' * 10)
+
 
 # A Pythagorean triple is a triple (a, b, c) of integer numbers satisfying the equation a2 + b2 = c2.

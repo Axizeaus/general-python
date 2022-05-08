824a5d23d1d36141e8a7e2f046880a937a478188
Axizeaus <axizeaus@gmail.com>
2022-05-08 21:24:32 +0630

    decorator improved

--- cpt6/decorator.py
+++ cpt6/decorator.py
@@ -2,10 +2,14 @@ from time import sleep, time
 def f(sleep_time = 0.1):
     sleep(sleep_time)
 
-def measure(func,*args, **kwargs):
-    t = time()
-    func(*args, **kwargs)
-    print(func.__name__, 'took', time() - t)
-    
-measure(f,1)
-measure(f,sleep_time = 0.5)
+def measure(func):
+    def wrapper(*args, **kwargs):
+        t = time()
+        func(*args, **kwargs)
+        print(func.__name__, 'took', time() - t)
+    return wrapper
+
+f = measure(f)
+f(0.2)
+f(sleep_time = 0.3)
+print(f.__name__)

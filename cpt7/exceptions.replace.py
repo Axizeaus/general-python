2dc72240bc411efd70f2dbe1dc7731039031ee10
Axizeaus <axizeaus@gmail.com>
2022-06-18 22:40:14 +0630

    unconventional exceptions

--- cpt7/exceptions.replace.py
+++ cpt7/exceptions.replace.py
@@ -5,6 +5,6 @@ vowels = {'a' : 11, 'e' : 12, 'i' : 13, 'o' : 14, 'u' : 15}
 
 try :
     pos = vowels['y']
-except NotFoundError:
-    raise NotFoundError(*NotFoundError.args)
+except KeyError as ke:
+    raise NotFoundError(*ke.args) from None
 
